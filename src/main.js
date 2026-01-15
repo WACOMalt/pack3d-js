6896d4e1e23b88a6414341f2162dfeccae665508
Ben <wacomalt@gmail.com>
2026-01-15 01:41:18 -0500

    Fix PWA paths for subdirectory hosting

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ console.log('Pack3D-JS initialized');
 // Register Service Worker for PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js', { scope: './' })
       .then(registration => {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
